d338bb5833cf57c6e674241b59f0aa7bb24445d3
nikennovirasari <nikennovirasari@gmail.com>
2016-10-18 07:09:04 +0800

    databasenya

---
+++ database/tong.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2016 at 01:08 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tong`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cabang`
+--
+
+CREATE TABLE IF NOT EXISTS `cabang` (
+`id` int(11) NOT NULL,
+  `nama_cabang` varchar(255) NOT NULL,
+  `lat` varchar(255) NOT NULL,
+  `lng` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `id_kel` char(4) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cabang`
+--
+
+INSERT INTO `cabang` (`id`, `nama_cabang`, `lat`, `lng`, `gambar`, `id_kel`) VALUES
+(6, 'satu', '-0.493106450025523', '117.1379501745605', 'gambar/e7789084a75be44465161a35fbb84743.jpg', '001'),
+(7, 'dua', '-0.4963678945973434', '117.1540863439941', 'gambar/ca4d818dff33c0b2ad50f568ea1f90f3.jpg', '002'),
+(13, 'mbuh', '-0.5138766745416949', '117.10893940185542', '../gambar/c54dcc042a5b4b5ea35ef73ec8b67ab5.jpg', '002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE IF NOT EXISTS `kelurahan` (
+  `id_kel` char(4) NOT NULL,
+  `nm_kel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelurahan`
+--
+
+INSERT INTO `kelurahan` (`id_kel`, `nm_kel`) VALUES
+('001', 'sempaja'),
+('002', 'sungai');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`) VALUES
+('keno', 'keno');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cabang`
+--
+ALTER TABLE `cabang`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+ ADD PRIMARY KEY (`id_kel`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cabang`
+--
+ALTER TABLE `cabang`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
